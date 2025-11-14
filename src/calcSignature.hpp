1ddaffd3c67d606c5c46309f9c8470a90677f5f5
Arthur Bourdon <arthur.bourdon@milliman.com>
2025-11-14 15:04:26 +0100

    Resolve rebase conflicts by keeping current changes - Accept all current changes in src/calcSignature.hpp and src/pythonsigs.cpp - Maintain final logic for signature computation - Defers application of subsequent commits that modify these files

--- src/calcSignature.hpp
+++ src/calcSignature.hpp
@@ -1017,6 +1017,8 @@ class AdjointSuffixSignature {
                 }
             }
         }
+        delete m_datacopy_ptr;
+        m_datacopy_ptr = nullptr;
 
         template<typename Numeric>
         void writeOut(Numeric* dest) const {
